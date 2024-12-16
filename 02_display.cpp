fa377cf776e4aba6ac7cdff7725101d11a174ba1
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2024-12-16 11:55:43 +0530

    16/12

---
+++ 02_display.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  cout<<"this is cpp code"<<endl;
+  cout<<"this is seconde code";
+  cout<<endl<<"this is third line";
+  cout<<endl<<12.45;
+  return 0;
+}
---
+++ 07_string.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  string num;
+  cout<<"enter a num : ";
+  getline(cin,num);
+  cout<<"value of num : "<<num<<endl;
+  return 0;
+}
