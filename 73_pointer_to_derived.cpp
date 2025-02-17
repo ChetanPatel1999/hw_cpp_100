9e4afde0f89ccc381711b5e1f2f625bd985c04a3
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-02-17 12:19:31 +0530

    17/02

---
+++ 73_pointer_to_derived.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+class base
+{
+protected:
+    int num1;
+
+public:
+    base(int a)
+    {
+        num1 = a;
+    }
+    void virtual display()// virtual method
+    {
+        cout << "num1 = " << num1 << endl;
+    }
+};
+class derived : public base
+{
+    int num2;
+
+public:
+    derived(int a, int b) : base(a)
+    {
+        num2 = b;
+    }
+    void display()
+    {
+        cout << "num1 : " << num1 << endl;
+        cout << "num2 : " << num2 << endl;
+    }
+};
+int main()
+{
+    base *ptr;
+    ptr= new base(456);
+    ptr->display();
+    
+    ptr=new derived(34,90);
+    ptr->display();
+    return 0;
+}
